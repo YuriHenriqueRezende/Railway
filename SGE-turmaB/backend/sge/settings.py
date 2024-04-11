efab088b893d25046653bc48b8fe103a13e652a9
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-04-11 20:29:21 -0300

    commit

--- SGE-turmaB/backend/sge/settings.py
+++ SGE-turmaB/backend/sge/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://localhost',
+                 'https://railway-production-c759.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost',
+                 'https://railway-production-c759.up.railway.app'
+]
 
 
 # Application definition
