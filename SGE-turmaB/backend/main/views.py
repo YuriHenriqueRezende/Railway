ea0fefa2088a7b63ed5b2fe80e86176d9b5269b9
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-04-18 22:16:38 -0300

    commit

--- SGE-turmaB/backend/main/views.py
+++ SGE-turmaB/backend/main/views.py
@@ -3,10 +3,16 @@ from .models import *
 from .serializers import *
 
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
+
+class DeadlineCustomPermission(BasePermission):
+    def has_object_permission(self, request, view):
+        return 
 
 # Create your views here.
 class DeadlineView(ModelViewSet):
     queryset = Deadline.objects.all()
     serializer_class = DeadlineSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (DeadlineCustomPermission,)
+
+    
