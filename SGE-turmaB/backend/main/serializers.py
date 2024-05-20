2c5a828c28970d970d7f29f11ef66a174bdfb8a3
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-05-20 20:39:09 -0300

    commit

--- SGE-turmaB/backend/main/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-#conversor python <---> json
-class DeadlineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Deadline
-        fields = '__all__'
-        many = True
---
+++ somativa/app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+CARGOS
+STATUS
+
+class fotos(models.Model):
+    link =
+
+class usuariosCustomizado(models.Model):
+    nome =
+    email =
+    cpf =
+    biografia =
+    cargo =
+
+class livro(models.Model):
+    titulo =
+    foto = 
+    descricao = 
+    numero_pagina =
+    formato =
+    numero_edicao =
+    autor =
+    ano_publicacao = 
+    categoria =
+    status =
+    preco =
+
+
+class emprestimo(models.Model):
+    data_inicio =
+    data_fim =
+
