bd91ed24aa70973e5f4650cc938e3d145273c4a4
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-05-21 00:28:51 -0300

    commit

--- somativa/app/models.py
+++ somativa/app/models.py
@@ -1,40 +1,85 @@
 from django.db import models
+from .user import Gerenciador  # type: ignore
+from django.forms import URLField
 
 # Create your models here.
-CARGOS
-STATUS
-FORMATOS
+CARGOS = [
+        ("A","ADM"),
+        ("AU","AUTOR"),
+        ("B","BIBLIOTECARIO(a)"),
+        ("U","USUARIO")
+]
 
-class fotos(models.Model):
+STATUS = [
+        ("P","Pendente"),
+        ("C","Cancelado(a)"),
+        ("A","Aprovado(a)")
+]
+
+FORMATOS = [
+        ("E","E-BOOK"),
+        ("F","FISICO")
+]
+
+
+class fotoslivro(models.Model):
     nome = models.CharField(max_length=50)
-    link = models.CharField(max_length=200)
+    link = URLField(max_length=200, verbose_name="Image URL")
 
     def __str__(self):
         return self.nome
 
 
 class usuariosCustomizado(models.Model):
-    nome =
-    email =
-    cpf =
-    biografia =
-    cargo =
+    nome = models.CharField(max_length=50)
+    email = models.EmailField("endereço de email", unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    cpf = models.CharField(max_length=20)
+    biografia = models.CharField(max_length=200)
+    cargo = models.CharField(max_length=100, choices=CARGOS, default="U")
+    foto = URLField(max_length=200, verbose_name="Image URL")
+
+    objects = Gerenciador()
+    REQUIRED_FIELDS = []
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class categorias(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
 
 class livro(models.Model):
-    titulo =
-    foto = 
-    descricao = 
-    numero_pagina =
-    formato =
-    numero_edicao =
-    autor =
-    publicacao = 
-    categoria =
-    status =
-    preco =
+    titulo = models.CharField(max_length=50)
+    imagemfk = models.ForeignKey(fotoslivro, related_name='fotosusuariosFKFK', on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=100)
+    usuarioFK = models.ForeignKey(usuariosCustomizado, related_name='fotosusuariosFKFK', on_delete=models.CASCADE)
+    numero_pagina = models.IntegerField()
+    formato = models.CharField(max_length=100, choices=FORMATOS)
+    numero_edicao = models.IntegerField()
+    autor = models.CharField(max_length=50)
+    publicacao = models.IntegerField()
+    categoriaFK = models.ForeignKey(categorias, related_name='categoriasFKFK', on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=FORMATOS, default="P")
+    preco = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.titulo
 
 
 class emprestimo(models.Model):
-    data_inicio =
-    data_fim =
+    livroFK = models.ForeignKey(livro, related_name="livroFKFK", on_delete=models.CASCADE)
+    data_inicio = createdDate = models.DateField(auto_now_add=True)
+    data_fim = models.DateField()
+
+    def __str__(self):
+        return self.livroFK
 
